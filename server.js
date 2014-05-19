9f7b01bf95197824a3aab18144020ef210f52c09
rcpeng34 <rcpeng@gmail.com>
2014-05-19 12:23:41 -0700

    Adds server response for bootstrap

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require ('express'), 
     http    = require ('http'), 
-    https   = require('https');
-    app = require(__dirname + '/app/app.js')
+    https   = require('https'),
+    fbgraph = require ('fbgraph'),
+    app = require(__dirname + '/app/app.js');
 
 var app = express();
 var httpport = process.env.port || 3005;
@@ -20,7 +21,11 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/html/index.html');
 });
+app.get('bower_components/bootstrap/dist/css/bootstrap.min.css', function(req, res){
+  res.sendfile(__dirname + 'bower_components/bootstrap/dist/css/bootstrap.min.css');
+});
+app.get('bower_components/bootstrap/dist/js/bootstrap.min.js', function(req, res){
+  res.sendfile(__dirname + 'bower_components/bootstrap/dist/js/bootstrap.min.js');
+});
 
 //****************** Begin FB graph stuff ***********//
-
-var fbgraph = require ('fbgraph');
