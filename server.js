23964e3936ab9c64c09407bc8c8e0fd2edf97d46
rcpeng34 <rcpeng@gmail.com>
2014-05-19 12:30:19 -0700

    Fixes bootstrap directory paths and adds more detailed server startup logging info

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var httpport = process.env.port || 3005;
 // var httpsport = 4001;
 
 var server = app.listen(httpport, function() {
-    console.log('Listening on port %d', server.address().port);
+    console.log('Listening on port %d', '|', server.address().port, '|' + typeof(server.address().port));
 });
 
 // Serve up content from public directory
@@ -22,10 +22,10 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/html/index.html');
 });
 app.get('bower_components/bootstrap/dist/css/bootstrap.min.css', function(req, res){
-  res.sendfile(__dirname + 'bower_components/bootstrap/dist/css/bootstrap.min.css');
+  res.sendfile(__dirname + '/bower_components/bootstrap/dist/css/bootstrap.min.css');
 });
 app.get('bower_components/bootstrap/dist/js/bootstrap.min.js', function(req, res){
-  res.sendfile(__dirname + 'bower_components/bootstrap/dist/js/bootstrap.min.js');
+  res.sendfile(__dirname + '/bower_components/bootstrap/dist/js/bootstrap.min.js');
 });
 
 //****************** Begin FB graph stuff ***********//
