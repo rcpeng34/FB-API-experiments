1b045037e620078a921c7a85fdcf0b1de89265e7
rcpeng34 <rcpeng@gmail.com>
2014-05-19 16:41:48 -0700

    Adds fb status calls

--- public/scripts/fb.js
+++ public/scripts/fb.js
@@ -5,26 +5,31 @@ define(['facebook'], function(){
   });
   // logs in and begins making api calls to paths indicated in apiCalls
   // this is for exploratory purposes
-  FB.login(function(response){
-    if (response.authResponse) { // response successful, do stuff
 
-      var apiCalls = ['/me/albums', '/me/photos', '/me/locations', '/photos/uploaded', '/me/statuses', '/me/tagged_places'];
-      for (var i = 0; i < apiCalls.length; i++) {
-        FB.api(apiCalls[i], function(response) {
-          console.log('response |', response);
-        });
-      }
+  // FB.login(function(response){
+  //   if (response.authResponse) { // response successful, do stuff
+
+  //     var apiCalls = ['/me/albums', '/me/photos', '/me/locations', '/photos/uploaded', '/me/statuses', '/me/tagged_places'];
+  //     for (var i = 0; i < apiCalls.length; i++) {
+  //       FB.api(apiCalls[i], function(response) {
+  //         console.log('response |', response);
+  //       });
+  //     }
+  //   } else {
+  //     console.log('User cancelled login or did not fully authorize.');
+  //   }
+  // }, {scope: ['user_location', 'user_photos', 'user_status', 'user_tagged_places', 'read_stream']}); // define permissions requested of the login
+
+  // Goal: store all status updates
+  FB.login(function(response){
+    if (response.authResponse){
+      FB.api('/me/statuses?limit=200', function(response){
+        console.log(response);
+        console.log('////////////////////////////////// paging');
+        console.log(response.paging.next);
+      });
     } else {
       console.log('User cancelled login or did not fully authorize.');
     }
-  }, {scope: ['user_location', 'user_photos', 'user_status', 'user_tagged_places', 'read_stream']}); // define permissions requested of the login
+  }, {scope:['user_status']});
 });
-
-// Goal: store all status updates
-FB.login(function(response){
-  if (response.authResponse){
-
-  } else {
-    console.log('User cancelled login or did not fully authorize.');
-  }
-}, {})
