5b72592f6a141d1ef69e761349a5faf0d3802457
rcpeng34 <rcpeng@gmail.com>
2014-05-19 15:43:28 -0700

    Includes fb graph api exploratory calls

--- public/scripts/fb.js
+++ public/scripts/fb.js
@@ -1,8 +1,29 @@
 define(['facebook'], function(){
+  // initializes fb call
   FB.init({
-    appId      : '711138455590961',
+    appId : '711138455590961',
   });
-  FB.getLoginStatus(function(response) {
-    console.log(response);
-  });
+  // logs in and begins making api calls
+  FB.login(function(response){
+    if (response.authResponse) { // response successful, do stuff
+
+      var apiCalls = ['/me/albums', '/me/photos', '/me/locations', '/photos/uploaded', '/me/statuses', '/me/tagged_places'];
+      for (var i = 0; i < apiCalls.length; i++) {
+        FB.api(apiCalls[i], function(response) {
+          console.log('response |', response);
+        });
+      }
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  }, {scope: ['user_location', 'user_photos', 'user_status', 'user_tagged_places', 'read_stream']}); // define permissions requested of the login
 });
+
+// Goal: store all status updates
+FB.login(function(response){
+  if (response.authResponse){
+
+  } else {
+    console.log('User cancelled login or did not fully authorize.');
+  }
+}, {})
