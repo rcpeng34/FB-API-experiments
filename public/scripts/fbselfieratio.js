f8dcdd41c70aae4f1452f177ad1b0105cdaa29c4
rcpeng34 <rcpeng@gmail.com>
2014-05-20 00:57:29 -0700

    Pulls all photos and sets pagination scaffolding

--- public/scripts/fbselfieratio.js
+++ public/scripts/fbselfieratio.js
@@ -1,4 +1,7 @@
 var photoArray = [];
+var photosByYou = 0;
+var photosByOthers = 0;
+var username;
 
 define(['facebook'], function(){
   // initializes fb call
@@ -7,17 +10,30 @@ define(['facebook'], function(){
   });
   FB.login(function(response){
     if(response.authResponse){
-      FB.api('/me/photos?limit=100', function(res){
+      FB.api('/me/photos?limit=400', function(res){
         photoArray.push(res.data);
-        $.ajax({
-          url: res.paging.next, complete: function(res) {
-            photoArray.push(JSON.parse(res.responseText).data);
-            console.log(res);
-          }
-        });
+        // with more time, this is where to begin logic to grab data from pagination
+        // note that total photos is only 400 so it's hardcoded for the purpose of the hackathon
+        // you can call getNextPage and break when the last item in the array has lengh < the limit in '/me/photos?limit=x'
+        console.log('Completed photo fetch');
       });
     } else {
        console.log('User cancelled login or did not fully authorize.');      
     }
   }, {scope: ['user_photos']});
 });
+
+
+// this is an incomplete method to grab data from the next page
+// return a bool on whether it is the last page and the next page's url if not null
+// it should accept as argument the array to push to
+// note this is not necessary for this purpose
+var getNextPage = function(nextURL) {
+  $.ajax({
+    url: res.paging.next, complete: function(res) {
+      photoArray.push(JSON.parse(res.responseText).data);
+      console.log('page get call returned and pushed');
+      return res.paging.next;
+    }
+  });
+};
