a933723080830ef29dcbb16e8a95fa96c631436a
rcpeng34 <rcpeng@gmail.com>
2014-05-19 20:04:33 -0700

    Adds helper function to convert array of status updates to array format required by jqcloud

--- public/scripts/fb.js
+++ public/scripts/fb.js
@@ -41,3 +41,30 @@ define(['facebook'], function(){
     }
   }, {scope:['user_status']});
 });
+
+// takes an array of status messages and returns an array of form required by jqcloud
+// this is the form required: [{text: 'lorem', weight: 14}...]
+var calcWordWeighting = function(statusArr) {
+  var resultObj = {};
+  var results = [];
+  for (var i = 0; i < statusArr.length; i++) {
+    console.log(statusArr[i]);
+    // for each message extrapolate list of words
+    var messageAsArray = statusArr[i]['message'].split(' ');
+    //for each word, check if it is in resultObj, if it is, increment
+    // if it is not, add it
+    for (var j = 0; j < messageAsArray.length; j++) {
+      var word = messageAsArray[j];
+      if(resultObj[word]) { // it exists
+        resultObj[word] += 1;
+      } else { // it does not exist yet
+        resultObj[word] = 1;
+      }
+    }
+  }
+  // now that resultObj is created, we must turn it into the right form for jqcloud
+  for (var key in resultObj) {
+    results.push({text: key, weight: resultObj[key]});
+  }
+  return results;
+};
