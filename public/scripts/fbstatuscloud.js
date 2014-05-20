1b89a7e2f3158496dc07504ac11bcdd704ed8894
rcpeng34 <rcpeng@gmail.com>
2014-05-20 15:32:57 -0700

    Fixes bug where cloud does not appear for users with too few repeated words

--- public/scripts/fbstatuscloud.js
+++ public/scripts/fbstatuscloud.js
@@ -38,13 +38,13 @@ define(['facebook'], function(){
         // statusArray is the correct form for pushLocationArray so call it here
         pushLocationArray(window.statusArray);
         window.weightedStatusArray = calcWordWeighting(window.statusArray);
-        $(function(){
-          var colwidth = $('.firstcol').width();
-          $('#wordcloud').jQCloud(window.weightedStatusArray, {
-            width: colwidth,
-            height: Math.max($('.firstcol').height(), 300)
-          });
+        
+        var colwidth = $('.firstcol').width();
+        $('#wordcloud').jQCloud(window.weightedStatusArray, {
+          width: colwidth,
+          height: Math.max($('.firstcol').height(), 300)
         });
+        
       });
     } else {
       console.log('User cancelled login or did not fully authorize.');
@@ -57,8 +57,13 @@ define(['facebook'], function(){
 var calcWordWeighting = function(statusArr) {
   var resultObj = {};
   var results = [];
+  // totalWords is the wordcount of all messages
+  //totalWords/# keys in resultObj will give mean weight
+  var totalWords = 0;
   for (var i = 0; i < statusArr.length; i++) {
     var messageAsArray = statusArr[i].message.split(' ');
+    // add # words in the message to totalWords
+    totalWords += messageAsArray.length;
     //for each word, check if it is in resultObj, if it is, increment
     // if it is not, add it
     for (var j = 0; j < messageAsArray.length; j++) {
@@ -70,9 +75,14 @@ var calcWordWeighting = function(statusArr) {
       }
     }
   }
+  // since there is lots of singletons, to make sure the cloud isn't too crowded
+  // and to protect against edge users who only use words once, find the mean weight
+  // use the mean weight to set min weight to display/return
+  var meanWeight = totalWords/Object.keys(resultObj).length;
+
   // now that resultObj is created, we must turn it into the right form for jqcloud
   for (var key in resultObj) {
-    if (resultObj[key] > 5){
+    if (resultObj[key] > meanWeight){
       results.push({text: key, weight: resultObj[key]});
     }
   }
