ee1762b0a9f799c9780156ebf46af0a53c01a6aa
rcpeng34 <rcpeng@gmail.com>
2014-06-10 00:03:23 -0700

    Initializes a map of the world on page

--- public/scripts/fblocationmap.js
+++ public/scripts/fblocationmap.js
@@ -1,4 +1,5 @@
 window.locationArray = [];
+window.map;
 
 // assume that input array allows access to location in form arr[x].place.location
 var pushLocationArray = function(inputArray) { 
@@ -10,3 +11,16 @@ var pushLocationArray = function(inputArray) {
   }
   console.log('finished pushing to locationArray');
 };
+
+var initialize = function(){
+  var mapOptions = {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 1
+  };
+  map = new google.maps.Map(document.getElementById('map-container'), mapOptions);
+  console.log('complete initialize');
+};
+
+$('document').ready(function(){console.log('in callback'); initialize();});
+
+// plot all the markers in locationArray as markers on map
