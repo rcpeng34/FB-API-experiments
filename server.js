1ab7cee733df08938a40a7dc112c6de591395f26
rcpeng34 <rcpeng@gmail.com>
2014-06-22 19:57:03 -0700

    Adds support page

--- server.js
+++ server.js
@@ -22,3 +22,7 @@ app.get('/', function(req, res) {
 app.get('/privacypolicy_termsofservice', function(req, res){
   res.sendfile(__dirname + '/public/html/privacytos.html');
 });
+
+app.get ('/support', function(req, res){
+  res.sendfile(__dirname + '/public/html/support');
+});
